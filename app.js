7572725d2504684e85230d83a6c341e882516d1b
Reenuay <unformedvoid777@gmail.com>
2016-10-30 00:16:44 +0400

    Firebase
    Firebase injected into project. Login template improved.

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-/*global angular*/
+/*global angular, firebase*/
 (function () {
   "use strict";
-  var app = angular.module("app", ["ui.router", "ui.bootstrap"]);
+	//Initialize Firebase
+  firebase.initializeApp({
+    apiKey: "AIzaSyDA88bCzk34u7ILqrj4fBljJs7WemmU3BA",
+    authDomain: "financemanager-42340.firebaseapp.com",
+    databaseURL: "https://financemanager-42340.firebaseio.com",
+    storageBucket: "financemanager-42340.appspot.com",
+    messagingSenderId: "172763067741"
+  });
+	
+  var app = angular.module("app", ["ui.router", "ui.bootstrap", "firebase"]);
 	
 	/*app config*/
 	app.config(function ($stateProvider) {
--- app/login/login.controller.js
+++ app/login/login.controller.js
@@ -3,6 +3,19 @@
 	"use strict";
 	var app = angular.module("app");
 	
-	app.controller("LoginController", function ($scope) {
-	});
+	app.controller("LoginController", ["$scope", "$firebaseAuth", function ($scope, $firebaseAuth) {
+		$scope.login = {};
+		$scope.login.email = "";
+		$scope.login.password = "";
+		
+		$scope.login.submit = function () {
+			$firebaseAuth().$signInWithEmailAndPassword($scope.login.email, $scope.login.password)
+			.then( function(firebaseUser) {
+				alert(JSON.stringify(firebaseUser));
+			})
+			.catch( function(error) {
+				alert(error.message);
+			});
+		};
+	}]);
 }());
