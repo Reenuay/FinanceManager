6b53dccadc731e21dfbcaef8daaa75b348798e4b
Reenuay <unformedvoid777@gmail.com>
2016-10-30 22:15:56 +0400

    Authentication
    Authentication procedure initialized.

--- app.js
+++ app.js
@@ -13,22 +13,28 @@
   var app = angular.module("app", ["ui.router", "ui.bootstrap", "ui-notification", "firebase", "jcs-autoValidate"]);
 	
 	/*app config*/
-	app.config([
-		"$stateProvider", 
-		"NotificationProvider",
-		function ($stateProvider, NotificationProvider) {
-		$stateProvider.state({
+	app.config(["$stateProvider", "NotificationProvider",
+	function ($stateProvider, NotificationProvider) {
+		//All possible routes
+		$stateProvider
+		.state({
 			name: "login",
 			url: "/login",
 			templateUrl: "app/login/login.template.html",
 			controller: "LoginController"
+		})
+		.state({
+			name: "main",
+			url: "/main",
+			templateUrl: "app/main/main.template.html"
 		});
 		
+		//Notification tool config
 		NotificationProvider.setOptions({
-			delay: 5000,
+			delay: 10000,
 			startTop: 20,
 			startRight: 10,
-			verticalSpacing: 20,
+			verticalSpacing: 10,
 			horizontalSpacing: 20,
 			positionX: "left",
 			positionY: "bottom",
@@ -36,7 +42,27 @@
 		});
 	}]);
 	
+	//Validation decorator config
 	app.run(['bootstrap3ElementModifier', function (bootstrap3ElementModifier) {
 		bootstrap3ElementModifier.enableValidationStateIcons(true);
 	}]);
+	
+	//Redirection on not-authenticated
+	app.run(function($rootScope, $location, $state, $firebaseAuth) {
+		$rootScope.$on( '$stateChangeStart', function (e, toState, toParams, fromState, fromParams) {
+			if(toState.name === "login"){
+				return; // no need to redirect 
+			}
+
+			if(!$firebaseAuth().$getAuth()) {
+				e.preventDefault();//stop current execution
+				$state.go('login');//go to login
+			}
+		});
+		
+		$firebaseAuth().$onAuthStateChanged( function (firebaseUser) {
+      if (!firebaseUser)
+				$state.go('login');//go to login
+    });
+	});
 }());
--- app/login/login.controller.js
+++ app/login/login.controller.js
@@ -3,7 +3,7 @@
 	"use strict";
 	var app = angular.module("app");
 	
-	app.controller("LoginController", ["$scope", "$firebaseAuth", "Notification", function ($scope, $firebaseAuth, Notification) {
+	app.controller("LoginController", ["$scope", "$firebaseAuth", "Notification", "$state", function ($scope, $firebaseAuth, Notification, $state) {
 		$scope.login = {};
 		$scope.login.email = "";
 		$scope.login.password = "";
@@ -11,7 +11,7 @@
 		$scope.login.submit = function () {
 			$firebaseAuth().$signInWithEmailAndPassword($scope.login.email, $scope.login.password)
 			.then( function (firebaseUser) {
-				Notification.success(JSON.stringify(firebaseUser));
+				$state.go("main");
 			})
 			.catch( function (error) {
 				Notification.error(error.message);
