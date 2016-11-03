8ce31a751e8275d374dd448ae48c452df7f308b2
Reenuay <unformedvoid777@gmail.com>
2016-11-03 16:40:38 +0400

    Forgot password
    When sending email automatically redirect to login page and shows
    notification.

--- app/forgotpassword/forgotpassword.controller.js
+++ app/forgotpassword/forgotpassword.controller.js
@@ -3,15 +3,15 @@
 	"use strict";
 	var app = angular.module("app");
 	
-	app.controller("ForgotpasswordController", ["$scope", "$firebaseAuth", "Notification", function ($scope, $firebaseAuth, Notification) {
+	app.controller("ForgotpasswordController", ["$scope", "$state","$firebaseAuth", "Notification", function ($scope, $state, $firebaseAuth, Notification) {
 		$scope.forgotpassword = {};
 		$scope.forgotpassword.email = "";
-		$scope.forgotpassword.sent = false;
 		
 		$scope.forgotpassword.send = function () {
 			$firebaseAuth().$sendPasswordResetEmail($scope.forgotpassword.email)
 			.then( function () {
-				$scope.forgotpassword.sent = true;
+				Notification.success("Email has been successfully sent.	Please check your email box.");
+				$state.go("login");
 			}).catch( function (error) {
 				Notification.error(error.message);
 			});
