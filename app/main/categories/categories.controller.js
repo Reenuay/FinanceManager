5503cd3d3fe862883ff43740d7d82f2c237d308e
Reenuay <unformedvoid777@gmail.com>
2016-11-16 14:41:45 +0400

    Validation
    Validation added to categories interface.

--- app/main/categories/categories.controller.js
+++ app/main/categories/categories.controller.js
@@ -3,14 +3,22 @@
 	"use strict";
 	var app = angular.module("app");
 	
-	app.controller("CategoriesController", ["$scope", "$http", "$firebaseArray", "$userData", function ($scope, $http, $firebaseArray, $userData) {
+	app.controller("CategoriesController", ["$scope", "$http", "$firebaseArray", "$userData", "Notification", function ($scope, $http, $firebaseArray, $userData, Notification) {
     $scope.categories = {};
 		$scope.categories.name = "";
 		$scope.categories.list = $firebaseArray($userData().child("categories"));
 		$scope.categories.color = "#000";
 		
 		$scope.categories.add = function () {
-			$scope.categories.list.$add({name: $scope.categories.name, icon: $scope.categories.icon, color: $scope.categories.color});
+			if ($scope.categories.name) {
+				$scope.categories.list.$add({
+					name: $scope.categories.name,
+					icon: $scope.categories.icon,
+					color: $scope.categories.color
+				});
+			} else {
+				Notification.error("Name is required.");
+			}
 		};
 		
 		$scope.categories.remove = function (item) {
