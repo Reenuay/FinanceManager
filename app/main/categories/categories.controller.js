530bd7316931a3544db6b8bee78f70691c619d28
Reenuay <unformedvoid777@gmail.com>
2017-01-31 22:30:30 +0400

    Improvement
    Improved code

--- app/main/categories/categories.controller.js
+++ app/main/categories/categories.controller.js
@@ -4,54 +4,48 @@
 	var app = angular.module("app");
 	
 	app.controller("CategoriesController", ["$scope", "$http", "$firebaseArray", "$userData", "Notification", "$error", function ($scope, $http, $firebaseArray, $userData, Notification, $error) {
-		//this is intermediate array that will be used in js but never seen by user
-		var tempList = [];
-		
-		//returns the parent count of given category
-		function parentCount($item) {
-			if (!$item)
-				return 0;
-			//get the parent of given item
-			var $parent = tempList.$getRecord($item.parent);
-			//if parent exists look for its parent
-			return $parent ? 1 + parentCount($parent) : 0;
-		}
-		
-		//this function will load categories. raw version will be stored in tempList and sorted will be in $scope.categories for user purposes
-		function loadCategories() {
-			(tempList = $firebaseArray($userData().child("categories")))
-			.$loaded().catch($error);
-		}
+		($scope.LoadCategories = function () {
+			($scope.categories = $firebaseArray($userData().child("categories")))
+			.$loaded().then(function () {
+				$scope.categories.forEach(function (value, index, array) {
+					value.level = ParentCount(value);
+				});
+			}).catch($error);
+		}());
 		
 		//sorts list so that the children elements follow after their parents
-		function sortLikeTree(list) {
+		$scope.Sort = function (list) {
 			if (!Array.isArray(list))
 				return;
 			if (list.length < 2)
 				return list;
+			//copy array
 			list = list.slice(0, list.length);
+			//sort by level
 			list.sort(function (a, b) {
 				var _a = a.level,
 						_b = b.level;
 				return (_a > _b) - (_a < _b);
 			});
+			//sort like tree
+			for (var i = 0; i < list.length; i++) {
+				for (var j = i + 1; j < list.length; j++) {
+					if (list[j].level !== list[i].level + 1)
+						continue;
+					list.splice(i + 1, 0, list.splice(j, 1)[0]);
+				}
+			}
 			return list;
+		};
+		
+		//returns the count of parents of given category
+		function ParentCount($item) {
+			if (!$item)
+				return 0;
+			//get the parent of given item
+			var $parent = $scope.categories.$getRecord($item.parent);
+			//if parent exists look for its parent
+			return $parent ? 1 + ParentCount($parent) : 0;
 		}
-			
-		//this function also needed in template
-		$scope.loadCategories = loadCategories;
-		//invoke this function to load categories at first time
-		loadCategories();
-		//watch the changes of the list
-		tempList.$watch(function () {
-			$scope.categories = sortLikeTree(tempList.map(function (el) {
-				return {
-					name: el.name,
-					color: el.color,
-					icon: el.icon,
-					level: parentCount(el)
-				};
-			}));
-		});
 	}]);
 }());
