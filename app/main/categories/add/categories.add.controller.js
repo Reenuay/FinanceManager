dcc1b2760e6de84494af9644d69f83200977a943
Reenuay <unformedvoid777@gmail.com>
2017-02-23 17:37:08 +0400

    Add/Edit interface done
    Addition and edition now is available. Intefrace is ready for use but
    code need to be improved(as always).

--- app/main/categories/add/categories.add.controller.js
+++ app/main/categories/add/categories.add.controller.js
@@ -3,20 +3,21 @@
 	"use strict";
 	var app = angular.module("app");
 	
-	app.controller("CategoriesAddController", ["$scope", "$stateParams", "$firebaseArray", "$userData", "Notification", "$error", "$colorList", "$iconList", function ($scope, $stateParams, $firebaseArray, $userData, Notification, $error, $colorList, $iconList) {
+	app.controller("CategoriesAddController", ["$scope", "$state", "$stateParams", "$firebaseArray", "$userData", "Notification", "$error", "$colorList", "$iconList", function ($scope, $state, $stateParams, $firebaseArray, $userData, Notification, $error, $colorList, $iconList) {
 		
 		/*
 			Scope variables.
 		*/
 		$scope.main = {};
+		$scope.main.new = {};
 		$scope.main.action = $stateParams.action;
 		
 		$scope.main.colors = $colorList();
 		$scope.main.icons = $iconList();
 		
 		if ($stateParams.action === "add") {
-			$scope.main.colors.active = $scope.main.colors[0];
-			$scope.main.icons.active = $scope.main.icons[0];
+			$scope.main.new.color = $scope.main.colors[0];
+			$scope.main.new.icon = $scope.main.icons[0];
 		}
 		
 		//Load parent category.
@@ -27,20 +28,52 @@
 			$scope.main.record = categories.$getRecord($stateParams.id);
 			if ($scope.main.record) {
 				if ($stateParams.action === "edit") {
-					$scope.main.colors.active = $scope.main.record.color;
-					$scope.main.icons.active = $scope.main.record.icon;
-					$scope.main.name = $scope.main.record.name;
+					$scope.main.new = $scope.main.record;
+				} else {
+					$scope.main.new.parent = $scope.main.record.$id;
 				}
 			}
 		})
 		.catch($error);
 		
 		$scope.main.SelectColor = function (color) {
-			$scope.main.colors.active = color;
+			$scope.main.new.color = color;
 		};
 		
 		$scope.main.SelectIcon = function (icon) {
-			$scope.main.icons.active = icon;
+			$scope.main.new.icon = icon;
 		};
+		
+		$scope.main.Submit = function () {
+			var name = $scope.main.new.name.toLowerCase(), i;
+			if ($stateParams.action === "add") {
+				//Check for uniqueness.
+				for (i = 0; i < categories.length; i++) {
+					if (name === categories[i].name.toLowerCase()) {
+						Notification.error("There is already such a category.");
+						return;
+					}
+				}
+				
+				//Add category.
+				categories.$add($scope.main.new)
+				.catch($error);
+			} else {
+				//Check for uniqueness.
+				for (i = 0; i < categories.length; i++) {
+					if (categories[i] !== $scope.main.new && name === categories[i].name.toLowerCase()) {
+						Notification.error("There is already such a category.");
+						return;
+					}
+				}
+				
+				//Update category.
+				categories.$save($scope.main.new)
+				.catch($error);
+			}
+			
+			$state.go("main.categories.list");
+		};
+		
 	}]);
 }());
